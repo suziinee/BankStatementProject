2d240878352e73cd733012ffe585080290ba052f
Sujin Jang <susan3722@snu.ac.kr>
2022-03-22 10:28:41 +0900

    Spring 첫 적용
    - Configuration, Bean annotation 적용하고 main에서 factory를 호출하는 방식을 변경
        - application context를 이용해서 호출함

--- src/main/java/dev/bank/bankstatement/BankStatementFactory.java
+++ src/main/java/dev/bank/bankstatement/BankStatementFactory.java
@@ -1,16 +1,23 @@
 package dev.bank.bankstatement;
 
-import dev.bank.bankstatement.data.BankStatementCSVParser;
-import dev.bank.bankstatement.data.BankStatementParser;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import dev.bank.bankstatement.data.BankStatementCSVParser;
+
+@Configuration //이 클래스가 bean 구성 정보로 활용될 수 있도록 지정
 public class BankStatementFactory {
     
+    @Bean //bean 구성정보 내 등록되어 있는 bean
     public BankStatementAnalyzer bankStatementAnalyzer() {
 
-        BankStatementParser bankStatementParser = new BankStatementCSVParser();
-        BankStatementAnalyzer bankStatementAnalyzer = new BankStatementAnalyzer(bankStatementParser);
-        
+        BankStatementAnalyzer bankStatementAnalyzer = new BankStatementAnalyzer(bankStatementCSVParser());
         return bankStatementAnalyzer;
+    }
+
+    @Bean
+    public BankStatementCSVParser bankStatementCSVParser() {
+        return new BankStatementCSVParser();
     }
 
 }
--- src/main/java/dev/bank/bankstatement/MainApplication.java
+++ src/main/java/dev/bank/bankstatement/MainApplication.java
@@ -2,14 +2,17 @@ package dev.bank.bankstatement;
 
 import java.io.IOException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class MainApplication {
     
     public static void main(String[] args) throws IOException {
         
-        BankStatementFactory bankStatementFactory = new BankStatementFactory();
-        BankStatementAnalyzer bankStatementAnalyzer = bankStatementFactory.bankStatementAnalyzer();
-        
+        final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(BankStatementFactory.class);
+        BankStatementAnalyzer bankStatementAnalyzer = applicationContext.getBean(BankStatementAnalyzer.class);
+
         bankStatementAnalyzer.analyze("bank-data-simple.csv");
     }
-    
+
 }
